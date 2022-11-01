2efd7a6d9e38fe20e6ed801fb9085330bfa56cfc
lagrange <654186749@qq.com>
2022-11-01 19:49:36 +0800

    2022.11.1

--- tracer_base/src/tracer_base_ros.cpp
+++ tracer_base/src/tracer_base_ros.cpp
@@ -15,16 +15,18 @@
 namespace westonrobot {
 TracerBaseRos::TracerBaseRos(std::string node_name)
     : rclcpp::Node(node_name), keep_running_(false) {
-  this->declare_parameter("port_name");   //声明参数
+    
+/***update for humble***/
+  this->declare_parameter("port_name", rclcpp::ParameterValue("can0"));   //声明参数
 
-  this->declare_parameter("odom_frame");
-  this->declare_parameter("base_frame");
-  this->declare_parameter("odom_topic_name");
+  this->declare_parameter("odom_frame", rclcpp::ParameterValue("odom"));
+  this->declare_parameter("base_frame", rclcpp::ParameterValue("base_link"));
+  this->declare_parameter("odom_topic_name", rclcpp::ParameterValue("odom"));
 
-  this->declare_parameter("is_tracer_mini");
-  this->declare_parameter("simulated_robot");
-  this->declare_parameter("control_rate");
-
+  this->declare_parameter("is_tracer_mini", rclcpp::ParameterValue(false));
+  this->declare_parameter("simulated_robot", rclcpp::ParameterValue(false));
+  this->declare_parameter("control_rate", rclcpp::ParameterValue(50));
+ /***update for humble***/
   LoadParameters();
 }
 
@@ -62,7 +64,7 @@ bool TracerBaseRos::Initialize() {
     std::cout << "Robot base: Tracer" << std::endl;
   }
 
-  ProtocolDectctor detector;
+  ProtocolDetector detector;
   if (detector.Connect(port_name_)) {
     auto proto = detector.DetectProtocolVersion(5);
   if (proto == ProtocolVersion::AGX_V2) {

fa0ac4fe222567833f8be127ccba45dd534b2167
Aleksandr Kashirin <kashirin.aleks@ya.ru>
2023-01-09 19:45:09 +0300

    ProtocolDectctor -> ProtocolDetector

--- tracer_base/src/tracer_base_ros.cpp
+++ tracer_base/src/tracer_base_ros.cpp
@@ -62,7 +62,7 @@ bool TracerBaseRos::Initialize() {
     std::cout << "Robot base: Tracer" << std::endl;
   }
 
-  ProtocolDectctor detector;
+  ProtocolDetector detector;
   if (detector.Connect(port_name_)) {
     auto proto = detector.DetectProtocolVersion(5);
   if (proto == ProtocolVersion::AGX_V2) {

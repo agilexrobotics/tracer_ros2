8196f9f19cc1a17c69619389d4a732a0129e7f1a
HoTing Tsai <hoting811121@gmail.com>
2023-01-19 15:55:56 +0800

    fix some bugs of build failed

--- tracer_base/src/tracer_base_ros.cpp
+++ tracer_base/src/tracer_base_ros.cpp
@@ -15,15 +15,15 @@
 namespace westonrobot {
 TracerBaseRos::TracerBaseRos(std::string node_name)
     : rclcpp::Node(node_name), keep_running_(false) {
-  this->declare_parameter("port_name");   //声明参数
+  this->declare_parameter("port_name", "can0");   //声明参数
 
-  this->declare_parameter("odom_frame");
-  this->declare_parameter("base_frame");
-  this->declare_parameter("odom_topic_name");
+  this->declare_parameter("odom_frame", "odom");
+  this->declare_parameter("base_frame", "base_link");
+  this->declare_parameter("odom_topic_name", "odom");
 
-  this->declare_parameter("is_tracer_mini");
-  this->declare_parameter("simulated_robot");
-  this->declare_parameter("control_rate");
+  this->declare_parameter("is_tracer_mini", false);
+  this->declare_parameter("simulated_robot", false);
+  this->declare_parameter("control_rate", 50);
 
   LoadParameters();
 }
